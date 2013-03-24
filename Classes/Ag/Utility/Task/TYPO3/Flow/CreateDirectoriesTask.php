2de102bd6598423ddf6537c5201dd7310e3bfe16
Henrik Møller Rasmussen <hmr@agitso.com>
2013-03-24 15:03:55 +0100

    Add some Surf tasks

---
+++ Classes/Ag/Utility/Task/TYPO3/Flow/CreateDirectoriesTask.php
@@ -0,0 +1,40 @@
+<?php
+namespace Ag\Utility\Task\TYPO3\Flow;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Surf".            *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A task to create TYPO3 Flow specific directories
+ *
+ */
+class CreateDirectoriesTask extends \TYPO3\Surf\Task\Generic\CreateDirectoriesTask {
+
+	/**
+	 * Execute this task
+	 *
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$options = array(
+			'directories' => array(
+				'shared/Data/Sessions/Persistent',
+				'shared/Data/Sessions/Session'
+			)
+		);
+		parent::execute($node, $application, $deployment, $options);
+	}
+}
+?>
---
+++ Classes/Ag/Utility/Task/TYPO3/Flow/SymlinkSessionTask.php
@@ -0,0 +1,63 @@
+<?php
+namespace Ag\Utility\Task\TYPO3\Flow;
+
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Surf".            *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A symlink task for linking the shared data directory
+ *
+ */
+class SymlinkSessionTask extends \TYPO3\Surf\Domain\Model\Task {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * Executes this task
+	 *
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$releaseIdentifier = $deployment->getReleaseIdentifier();
+		$releasesPath = $application->getDeploymentPath() . '/releases';
+		$commands = array(
+			"mkdir -p $releasesPath/$releaseIdentifier/Data/Temporary/Production/Cache/Data",
+			"cd $releasesPath/$releaseIdentifier/Data/Temporary/Production/Cache/Data",
+			"ln -sf ../../../../../../../shared/Data/Sessions/Persistent Login_Persistent",
+			"ln -sf ../../../../../../../shared/Data/Sessions/Session Login_Session"
+		);
+		$this->shell->executeOrSimulate($commands, $node, $deployment);
+	}
+
+	/**
+	 * Simulate this task
+	 *
+	 * @param Node $node
+	 * @param Application $application
+	 * @param Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function simulate(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		$this->execute($node, $application, $deployment, $options);
+	}
+
+}
+?>
